aa4f7c57bb938d94eaa9572f88a35930d695b01a
inquntum <inquntum@gmail.com>
2026-02-07 09:45:50 +0800

    function that creates and populates table worked

--- db/populatedb.js
+++ db/populatedb.js
@@ -3,11 +3,31 @@ import { Client } from "pg";
 const SQL = `
  CREATE TABLE IF NOT EXISTS userPost (
   msgId INTEGER PRIMARY KEY GENERATED ALWAYS AS IDENTITY ,
-  username VARCHAR(15),
-  post VARCHAR(300)
+  username VARCHAR(15) NOT NULL CHECK (LENGTH(TRIM(username)) > 0),
+  content VARCHAR(300) NOT NULL CHECK (LENGTH(TRIM(content)) > 0)
 );
 
-INSERT INTO userPost (username, post)
+INSERT INTO userPost (username, content)
 VALUES ('Kyle1111111', 'I love cheese'),
 		   ('Kyle', 'I love cheese');
  `
+
+async function populateDB() {
+  console.log("seeding...")
+
+  const client = new Client({
+    connectionString: process.argv[2]
+  });
+
+  try {
+    await client.connect();
+    await client.query(SQL);
+    console.log("Done");
+  } catch(err) {
+    console.log(`Seeding error: ${err}`)
+  } finally {
+    await client.end();
+  }
+}
+
+populateDB()
